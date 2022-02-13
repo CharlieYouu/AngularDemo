786def3d46fd888c0d026597a2002a1c0be43686
CharlieYo <it1164874@126.com>
2022-02-13 14:08:15 +0800

    Revert "angular サンポル　作成"
    This reverts commit f643ef4b311937c6b569e474b5867eca2fb543c9.

--- src/app/directive/directive.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { SpyDirective } from './spy.directive';
-import { HightLightDirective } from './hightLight.directive';
-
-@NgModule({
-  declarations: [SpyDirective, HightLightDirective],
-  exports: [SpyDirective, HightLightDirective],
-})
-export class DirectiveModule {}
